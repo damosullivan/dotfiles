b6e7bc59dd2eedcfd53eed62306ddc4198fce1be
Damien O'Sulivan <damosullivan@gmail.com>
2014-11-03 16:44:08 +0000

    Added in package control settings/list to sublime folder

--- activate.sh
+++ activate.sh
@@ -9,6 +9,7 @@ cd ~
 mv .vimrc $CURRENT/backup
 mv .vim $CURRENT/backups
 mv Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings $CURRENT/backups
+mv Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings $CURRENT/backups
 
 # Sym Link
 ln -s $CURRENT/vimrc .vimrc
@@ -16,6 +17,7 @@ ln -s $CURRENT/vim .vim
 
 cd Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 ln -s ~/dotfiles/sublime/Preferences.sublime-settings Preferences.sublime-settings
+ln -s ~/dotfiles/sublime/Package\ Control.sublime-settings Package\ Control.sublime-settings
 
 #Back to dotfiles dir
 cd $CURRENT
